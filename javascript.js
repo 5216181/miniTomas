1dc1a002a5debd05a8a02d35c3833724993d6277
5216181 <51808709+5216181@users.noreply.github.com>
2024-10-30 22:19:40 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -5,6 +5,8 @@ Blockly.Arduino['encodemotor'] = function(block) {
   var value_e3 = Blockly.Arduino.valueToCode(block, 'e3', Blockly.Arduino.ORDER_ATOMIC);
   var value_e4 = Blockly.Arduino.valueToCode(block, 'e4', Blockly.Arduino.ORDER_ATOMIC);
   // TODO: Assemble Arduino into code variable.
+  Blockly.Arduino.definitions_.encodemotor = "\nvoid mqttCallback() {} \n\n";
+  /*
   var code = '\n';
 code=code + 'volatile long '+ value_e2.substr(1,value_e2.length-2) + ' = 0\n';
 
@@ -26,4 +28,5 @@ code=code + '    }\n';
 code=code + '  }\n';
 
   return code;
+  */
 };
