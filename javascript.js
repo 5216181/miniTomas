0ba9a278c365429806746fd03a3c91ad2e558a2f
5216181 <51808709+5216181@users.noreply.github.com>
2024-11-01 00:14:08 +0800

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+
 Blockly.Arduino['encodemotor'] = function(block) {
   var value_e1 = Blockly.Arduino.valueToCode(block, 'e1', Blockly.Arduino.ORDER_ATOMIC);
   var value_e2 = Blockly.Arduino.valueToCode(block, 'e2', Blockly.Arduino.ORDER_ATOMIC);
@@ -41,34 +42,5 @@ code=code + '  }\n';
 */
   
 Blockly.Arduino.definitions_.func_encodemotor = code;
-return "";
-};
-
-//----------------------------------------------------------------------------------------------
-Blockly.Arduino['encodemotor_type2'] = function(block) {
-  var value_e2 = Blockly.Arduino.valueToCode(block, 'e2', Blockly.Arduino.ORDER_ATOMIC);
-  var value_e3 = Blockly.Arduino.valueToCode(block, 'e3', Blockly.Arduino.ORDER_ATOMIC);
-  var value_e4 = Blockly.Arduino.valueToCode(block, 'e4', Blockly.Arduino.ORDER_ATOMIC);
-
-  
-  var code = '\n' +
-//code=code + 'volatile long '+ value_e2.substr(1,value_e2.length-2) + ' = 0\n\n' +
-// 判斷方向1
- 'void encoderISR() {\n' +
- '  if (digitalRead('+value_e3.toString()+') == HIGH) {\n' +
- '    if (digitalRead('+value_e4.toString()+') == LOW) {\n' +
- '      '+value_e2+'++;  // 順時針\n' +
- '    } else {\n' +
- '      '+value_e2+'--;  // 逆時針\n' +
- '    }\n' +
- '  } else {\n' +
- '    if (digitalRead('+value_e4.toString()+') == LOW) {\n' +
- '      '+value_e2+'--;  // 逆時針\n' +
- '    } else {\n' +
- '      '+value_e2+'++;  // 順時針\n' +
- '    }\n' +
- '  }\n';  
-Blockly.Arduino.definitions_.func_encodemotor_type2 = code;
-
-return 'attachInterrupt('+value_e3.toString()+', encoderISR, CHANGE);\n';
+return "this is a test";
 };
--- toolbox.js
+++ toolbox.js
@@ -5,6 +5,4 @@ if (typeof toolbox_custom == 'undefined')
 
 toolbox_custom.push(''
 +'<category id="category_custom" name="miniTomas" colour="100">'
-+'<block type="encodemotor_type2"/>'
-+'<block type="encodemotor"/>'
-+'</category>');
++'<block type="encodemotor"/></category>');
