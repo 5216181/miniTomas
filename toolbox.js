772385ee96bb32b1c4e7c419030a0245480aa00c
5216181 <51808709+5216181@users.noreply.github.com>
2024-11-14 19:06:01 +0800

    Update toolbox.js

--- toolbox.js
+++ toolbox.js
@@ -7,5 +7,5 @@ toolbox_custom.push(''
 +'<category id="category_custom" name="miniTomas" colour="100">'
 +'<block type="encodemotor_type2"/>'
 +'<block type="encodemotor"/>'
-+'<block type="pinmode1114"/>'                    
++'<block type="pinmode1114"/>'            
 +'</category>');
