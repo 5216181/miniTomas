a1ef75e25cfde4bcdf335e780fd5d0c2cb02ccba
5216181 <51808709+5216181@users.noreply.github.com>
2024-11-18 17:44:05 +0800

    Update blocks.js

--- blocks.js
+++ blocks.js
@@ -1,3 +1,45 @@
+Blockly.Blocks['pinmode1114'] = {
+  init: function() {
+    this.appendValueInput("i1")
+        .setCheck("Number")
+        .appendField("宣告腳位");
+    this.appendDummyInput()
+        .appendField("為");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([["輸入INPUT","INPUT"], ["輸出OUTPUT","OUTPUT"]]), "i2");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(50);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+
+Blockly.Blocks['encodemotor'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("編碼馬達");
+    this.appendValueInput("e1")
+        .setCheck("String")
+        .appendField("函式名稱");
+    this.appendValueInput("e2")
+        .setCheck(null)
+        .appendField("回傳值名稱");
+    this.appendValueInput("e3")
+        .setCheck("Number")
+        .appendField("腳位一");
+    this.appendValueInput("e4")
+        .setCheck("Number")
+        .appendField("腳位二");
+    this.setInputsInline(true);
+    this.setColour(0);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
 Blockly.Blocks['encodemotor_type2'] = {
   init: function() {
     this.appendDummyInput()
@@ -15,47 +57,6 @@ Blockly.Blocks['encodemotor_type2'] = {
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(300);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
-//-------------------------------------------------------------------------------------------
-Blockly.Blocks['encodemotor'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("編碼馬達");
-    this.appendValueInput("e1")
-        .setCheck("String")
-        .appendField("函式名稱");
-    this.appendValueInput("e2")
-        .setCheck("String")
-        .appendField("回傳值名稱");
-    this.appendValueInput("e3")
-        .setCheck("Number")
-        .appendField("腳位一");
-    this.appendValueInput("e4")
-        .setCheck("Number")
-        .appendField("腳位二");
-    this.setInputsInline(true);
-    this.setColour(0);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
-//-----------------------------------------------------------------------------------------------
-Blockly.Blocks['pinmode1114'] = {
-  init: function() {
-    this.appendValueInput("i1")
-        .setCheck("Number")
-        .appendField("宣告腳位");
-    this.appendDummyInput()
-        .appendField("為");
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldDropdown([["輸入INPUT","INPUT"], ["輸出OUTPUT","OUTPUT"]]), "i2");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(50);
  this.setTooltip("");
  this.setHelpUrl("");
   }
