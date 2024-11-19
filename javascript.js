44e146a7d59feb54cd908c98932dd67f093ad213
5216181 <51808709+5216181@users.noreply.github.com>
2024-11-19 18:24:44 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+/*
 Blockly.Arduino['pinmode1114'] = function(block) {
   var value_i1 = Blockly.Arduino.valueToCode(block, 'i1', Blockly.Arduino.ORDER_ATOMIC);
   var dropdown_i2 = block.getFieldValue('i2');
@@ -5,7 +6,7 @@ Blockly.Arduino['pinmode1114'] = function(block) {
   var code = '...;\n';
   return code;
 };
-
+*/
 Blockly.Arduino['encodemotor'] = function(block) {
   var value_e1 = Blockly.Arduino.valueToCode(block, 'e1', Blockly.Arduino.ORDER_ATOMIC);
   var value_e2 = Blockly.Arduino.valueToCode(block, 'e2', Blockly.Arduino.ORDER_ATOMIC);
