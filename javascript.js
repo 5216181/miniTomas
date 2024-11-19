9afb81a7b9b03aa9d9c95a6b46d7719c34492546
5216181 <51808709+5216181@users.noreply.github.com>
2024-11-19 18:29:27 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-/*
+
 Blockly.Arduino['pinmode1114'] = function(block) {
   var value_i1 = Blockly.Arduino.valueToCode(block, 'i1', Blockly.Arduino.ORDER_ATOMIC);
   var dropdown_i2 = block.getFieldValue('i2');
@@ -6,22 +6,25 @@ Blockly.Arduino['pinmode1114'] = function(block) {
   var code = '...;\n';
   return code;
 };
-*/
+
+
+//--------------------------------------------------------------------------------
 Blockly.Arduino['encodemotor'] = function(block) {
   var value_e1 = Blockly.Arduino.valueToCode(block, 'e1', Blockly.Arduino.ORDER_ATOMIC);
   var value_e2 = Blockly.Arduino.valueToCode(block, 'e2', Blockly.Arduino.ORDER_ATOMIC);
   var value_e3 = Blockly.Arduino.valueToCode(block, 'e3', Blockly.Arduino.ORDER_ATOMIC);
   var value_e4 = Blockly.Arduino.valueToCode(block, 'e4', Blockly.Arduino.ORDER_ATOMIC);
   // TODO: Assemble Arduino into code variable.
-  var code = 'P1...;\n';
+  var code = 'P3...;\n';
   return code;
 };
 
+//--------------------------------------------------------------------------------------
 Blockly.Arduino['encodemotor_type2'] = function(block) {
   var value_e2 = Blockly.Arduino.valueToCode(block, 'e2', Blockly.Arduino.ORDER_ATOMIC);
   var value_e3 = Blockly.Arduino.valueToCode(block, 'e3', Blockly.Arduino.ORDER_ATOMIC);
   var value_e4 = Blockly.Arduino.valueToCode(block, 'e4', Blockly.Arduino.ORDER_ATOMIC);
   // TODO: Assemble Arduino into code variable.
-  var code = 'P2...;\n';
+  var code = 'P4...;\n';
   return code;
 };
