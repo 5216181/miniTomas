b300bd7f7ced3a2ed22c3338bab6b82055e46b3c
5216181 <51808709+5216181@users.noreply.github.com>
2025-04-24 19:58:59 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -35,7 +35,7 @@ Blockly.Arduino['encodemotor'] = function(block) {
 Blockly.Arduino.definitions_.func_encodemotor = code;
 //Blockly.Arduino.setups_["print_abc"] = 'Serial.println("abc");'; 
 //Blockly.Arduino.setups_["print_attachInterrupt"]='attachInterrupt('+value_e3.toString()+','+value_e1.substr(1,value_e1.length-2)+', CHANGE);\n';
-//return 'attachInterrupt('+value_e3.toString()+','+value_e1.substr(1,value_e1.length-2)+', CHANGE);\n';
+return 'attachInterrupt('+value_e3.toString()+','+value_e1.substr(1,value_e1.length-2)+', CHANGE);\n';
 };
 
 //--------------------------------------------------------------------------------------
