a7e59abdbbe1edfb34a974462a71d77bb7cf6a1d
5216181 <51808709+5216181@users.noreply.github.com>
2025-04-24 20:17:29 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -36,6 +36,7 @@ Blockly.Arduino.definitions_.func_encodemotor = code;
 Blockly.Arduino.setups_.setup_neopixel_begin="pixels.begin();\n";
 //Blockly.Arduino.setups_["print_attachInterrupt"]='attachInterrupt('+value_e3.toString()+','+value_e1.substr(1,value_e1.length-2)+', CHANGE);\n';
 //return 'attachInterrupt('+value_e3.toString()+','+value_e1.substr(1,value_e1.length-2)+', CHANGE);\n';
+  return "";
 };
 
 //--------------------------------------------------------------------------------------
